57e19e4435835649a62e47fbf754b77d5b695bae
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-15 00:27:53 -0300

    primeiros passos em python :)

---
+++ 01/s.py
@@ -0,0 +1,35 @@
+print(8+2) # sem o uso das apaso python vai somar os operadores
+print('ola, estou dando meus primeiros passos na carreira de progamador back end.') # tpda mensagem tem q estar entre aspas
+print('4'+'34') #com os usos das apas o python vai juntar os valores
+print('ola' , 5)  
+
+nome = 'samuel'
+idade = 17
+peso = 45.8
+print(nome, idade, peso)
+
+nome = input('qual o seu nome?')
+idade = input('quantos anos vc tem ?')
+peso = input('qual é o seu peso?')
+print(nome , idade , peso)
+
+nome = input('qual o seu nome')
+idade = input('qual a sua idade')
+peso = input('qual o seu peso')
+
+print(nome , idade , peso)
+
+nome = input('qual é o seu nome ?')
+print('seja bem vindo', nome)
+
+dia = input('em que dia vc nasceu ?')
+mes = input('em que mes vc nasceu ?')
+ano = input('em que ano vc nasceu ?')
+print('ola, vc nasceu no', dia, 'de', mes, 'de', ano,'.' 'correto?')
+
+
+numero1 = input('digite um numero')
+numero2 = input('digite outro numero')
+soma = int(numero1) + int(numero2)
+print('A soma é:', soma )
+
