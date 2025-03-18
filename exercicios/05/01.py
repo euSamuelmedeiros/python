a95d8e08530b55413741904683421447827763d5
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-18 00:22:12 -0300

    :)

---
+++ exercicios/05/01.py
@@ -0,0 +1,30 @@
+
+#005 desafio
+"""n = int(input('digite um numero: '))
+a = n - 1
+s  = n + 1
+print('analisando o valor {}, seu antecessor é {}, e o seu sucessor é {}'.format(n, a, s))"""
+
+"""n = int(input('digite um numero: '))
+a = n - 1
+s = n + 1
+print('olhando o valor {}, seu antecessor é {}, e seu antecessor é {}'.format(n, a, s))"""
+
+#006 desafio
+"""n = int(input('digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** n
+print('o dobro de è {}'.format(d))
+print('p triplo de {} é {}'.format(n, t))
+print('a raiz quadrada de {} é igual a {}'.format(n, r))"""
+
+
+
+#007 desafio
+
+
+
+
+
+
