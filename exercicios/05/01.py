e1e0c72b78ada5e7d39e2ee89ea0725c87a0ec86
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-18 21:35:05 -0300

    :)

--- exercicios/05/01.py
+++ exercicios/05/01.py
@@ -14,14 +14,18 @@ print('olhando o valor {}, seu antecessor é {}, e seu antecessor é {}'.format(
 """n = int(input('digite um numero: '))
 d = n * 2
 t = n * 3
-r = n ** n
+r = n ** (1/2)
 print('o dobro de è {}'.format(d))
 print('p triplo de {} é {}'.format(n, t))
 print('a raiz quadrada de {} é igual a {}'.format(n, r))"""
 
-
-
 #007 desafio
+
+n1 = float(input('primeira nota do aluno:'))
+n2 = float(input('segunda nota do aluno:'))
+s = n1 + n2 
+m = s / 2
+print('a media do aluno é: {}'.format(m))
 
 
 
