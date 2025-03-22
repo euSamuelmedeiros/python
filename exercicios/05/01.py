79f05723e6b215ed9814427be5f54f342788a7b1
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-22 17:42:03 -0300

    :)

--- exercicios/05/01.py
+++ exercicios/05/01.py
@@ -21,13 +21,19 @@ print('a raiz quadrada de {} é igual a {}'.format(n, r))"""
 
 #007 desafio
 
-n1 = float(input('primeira nota do aluno:'))
+"""n1 = float(input('primeira nota do aluno:'))
 n2 = float(input('segunda nota do aluno:'))
 s = n1 + n2 
 m = s / 2
-print('a media do aluno é: {}'.format(m))
+print('a media do aluno é: {}'.format(m))"""
+
+#008 desafio
 
 
+n = int(input('digite um numero: '))
+a = n - 1
+b = n + 1
+input('alanisando o valor {} seu antecessor é {} e seu sucessor é {}'.format(n, a, b))
 
 
 
