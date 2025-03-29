1b48372473ae0bbeb77f88c001bd550e1fd380e6
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-03-29 16:53:53 -0300

    Update 01.py

--- exercicios/05/01.py
+++ exercicios/05/01.py
@@ -1,35 +1,35 @@
 
 #005 desafio
-n = int(input('digite um numero: '))
+"""n = int(input('digite um numero: '))
 a = n - 1
 s  = n + 1
-print('analisando o valor {}, seu antecessor é {}, e o seu sucessor é {}'.format(n, a, s))
+print('analisando o valor {}, seu antecessor é {}, e o seu sucessor é {}'.format(n, a, s))"""
 
-n = int(input('digite um numero: '))
+"""n = int(input('digite um numero: '))
 a = n - 1
 s = n + 1
-print('olhando o valor {}, seu antecessor é {}, e seu antecessor é {}'.format(n, a, s))
+print('olhando o valor {}, seu antecessor é {}, e seu antecessor é {}'.format(n, a, s))"""
 
 #006 desafio
-n = int(input('digite um numero: '))
+"""n = int(input('digite um numero: '))
 d = n * 2
 t = n * 3
 r = n ** (1/2)
 print('o dobro de è {}'.format(d))
 print('p triplo de {} é {}'.format(n, t))
-print('a raiz quadrada de {} é igual a {}'.format(n, r))
+print('a raiz quadrada de {} é igual a {}'.format(n, r))"""
 
 #007 desafio
 
-n1 = float(input('primeira nota do aluno:'))
+"""n1 = float(input('primeira nota do aluno:'))
 n2 = float(input('segunda nota do aluno:'))
 s = n1 + n2 
 m = s / 2
-print('a media do aluno é: {}'.format(m))
+print('a media do aluno é: {}'.format(m))"""
 
 #009 desafio tabuada de um numero inteiro
 
-num = int(input('Digite um numero apra ver a sua tabuada: '))
+"""num = int(input('Digite um numero apra ver a sua tabuada: '))
 print('------------')
 print('{} x {} = {}'.format(num, 1, num*1))
 print('{} x {} = {}'.format(num, 2, num*2))
@@ -41,7 +41,11 @@ print('{} x {} = {}'.format(num, 7, num*7))
 print('{} x {} = {}'.format(num, 8, num*8))
 print('{} x {} = {}'.format(num, 9, num*9))
 print('{} x {} = {}'.format(num, 10, num*10))
-print('------------')
+print('------------')"""
+
+#010 desafio
+
+
 
 
 
