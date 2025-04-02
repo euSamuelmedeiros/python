294f783aadd1ddba20c1d1ec0aa2b8ec997db294
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-04-02 20:51:38 -0300

    :)

--- treinos/01/02/index2.py
+++ treinos/01/02/index2.py
@@ -7,17 +7,20 @@ if op == '+':
 elif op == '-':
         res = n1 - n2
 elif op == '*':
-        res = n1 * n2
-if n2 != 0:
-     res = n1 / n2
-else:
-     res = 'erro, divisão por zero'
-else:
-res = 'Erro, operação por zero'
-         
+     res = n1 * n2
+elif op == '/':
+     if n2 != 0:
+          res = n1 / n2
+     else:
+          res = 'zero'
+else: 
+     res = 'operaçao por zero'
+     print('o resulrado entre {} e {} é {}'.format(n1, n2, res))
 
 
-n1 = float(input('number 1: '))
+         # o de baixo funciona
+
+"""n1 = float(input('number 1: '))
 n2 = float(input('number 2: '))
 op = input("ecolha a operação (+, -, *, / ):")
 if op == '+':
@@ -33,7 +36,7 @@ elif op == '/':
         resul = 'erro divisão por 0'
 else:
     resul = ' operaçao por zero'
-print('o resulrado entre {} e {} é {}'.format(n1, n2, resul))
+print('o resulrado entre {} e {} é {}'.format(n1, n2, resul))"""
 
 
 
