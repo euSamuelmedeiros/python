5730fe4c2344ea770c9bfa65567c4ec7ceb3057b
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-04-19 22:56:36 -0300

    .

---
+++ aulas/aula 13/index.py
@@ -0,0 +1,20 @@
+"""for c in range(10, 0, -1):  #'for' --> estrututa de repetição
+    print(c)
+print('FIM')"""
+
+"""n1 = int(input('number one: '))
+for c in range(0, n1+1):
+    print(c)
+print('FIMMM')"""
+
+i = int(input('inicio: '))
+f = int(input('fim: '))
+p = int(input('passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('fimmmm')
+
+
+
+
+
